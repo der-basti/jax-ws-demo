da7fd69bd2cc9a12f0c7a364619ac31daa8e135a
Christopher Schulze <sandiego.mobile@googlemail.com>
2015-05-30 17:46:15 +0200

    client: image file upload

--- client/src/main/java/io/jax/ws/demo/client/DetailManagedBean.java
+++ client/src/main/java/io/jax/ws/demo/client/DetailManagedBean.java
@@ -9,13 +9,20 @@ import io.server.ws.App;
 import io.server.ws.AppService;
 import io.server.ws.AppServiceService;
 import io.server.ws.ReturnCode;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Base64;
 import java.util.Base64.Encoder;
+import java.util.Scanner;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.servlet.http.Part;
 
 import javax.xml.ws.soap.MTOMFeature;
+import sun.misc.IOUtils;
+
 
 /**
  *
@@ -29,6 +36,7 @@ public class DetailManagedBean implements Serializable {
     private App app;
     private AppService appServicePort;
     private String output;
+    private Part imageFile;
 
     /**
      * Creates a new instance of DetailManagedBean
@@ -68,20 +76,66 @@ public class DetailManagedBean implements Serializable {
     }
 
      public String create() {
-        return this.update();
+        this.update();
+        
+        
+        return null;
     }
     
     public String update() {
-        try {
-            ReturnCode returnCode;
+        ReturnCode returnCode;
+        
+        // update Name, Description and Price
+        try {         
             returnCode = appServicePort.update(app.getId(), app.getName(), app.getDescription(), app.getPrice());
             this.output = returnCode.toString();
         } catch (Exception e) {
             this.output = "Update/Create: " + ReturnCode.INTERNAL_ERROR.toString();
         }
+        
+        // update Image
+        if(imageFile!=null){
+        try{
+            //String fileContent = new Scanner(imageFile.getInputStream()).useDelimiter("\\A").next();
+            //System.out.println(fileContent);
+            InputStream inputStream = imageFile.getInputStream();   
+            byte[] bytes = IOUtils.readFully(inputStream,-1,true);
+//        FileOutputStream outputStream = new FileOutputStream(getFilename(imageFile));  
+//          
+//        byte[] buffer = new byte[4096];          
+//        int bytesRead = 0;  
+//        while(true) {                          
+//            bytesRead = inputStream.read(buffer);  
+//            if(bytesRead > 0) {  
+//                outputStream.write(buffer, 0, bytesRead);  
+//            }else {  
+//                break;  
+//            }                         
+//        }  
+//        outputStream.close();  
+        inputStream.close();  
+            
+        returnCode = appServicePort.uploadImage(app.getId(), bytes);
+        this.output = output+"\n File Upload: " + returnCode;
+        
+        
+        }catch(IOException e){
+            this.output = this.output+"\n Could not get fileContent";
+        }
+        }
 
         return null;
     }
+    
+    private static String getFilename(Part part) {  
+        for (String cd : part.getHeader("content-disposition").split(";")) {  
+            if (cd.trim().startsWith("filename")) {  
+                String filename = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");  
+                return filename.substring(filename.lastIndexOf('/') + 1).substring(filename.lastIndexOf('\\') + 1); // MSIE fix.  
+            }  
+        }  
+        return null;  
+    }  
 
     public String delete() {
         ReturnCode deleteReturn = appServicePort.delete(this.id);
@@ -118,4 +172,13 @@ public class DetailManagedBean implements Serializable {
         this.output = output;
     }
 
+    public Part getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Part imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    
 }
