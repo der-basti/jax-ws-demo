4b699768de3339fb8b823687cc46fb0fe7f23420
Sebastian <s7n@roadrunner.local>
2015-05-31 09:06:15 +0200

    fix #3 (remove required)

--- server/src/main/java/io/server/ws/model/App.java
+++ server/src/main/java/io/server/ws/model/App.java
@@ -4,7 +4,6 @@ import java.awt.Image;
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,25 +24,18 @@ public class App {
 	@Setter(value = AccessLevel.NONE)
 	private Long id;
 
-	@XmlElement(required = true)
 	private String name;
 
-	@XmlElement(required = true)
 	private String description;
 
-	@XmlElement(required = true)
 	private Double price;
 
-	@XmlElement(required = true)
 	private boolean activated;
 
-	@XmlElement(required = true)
 	private Date addDate;
 
-	@XmlElement(required = true)
 	private String appUrl;
 
-	@XmlElement(required = true)
 	private String checksum;
 
 	private Image image;
